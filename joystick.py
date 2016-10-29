5a4aabc533d70d4787e3c68c2edc5d01089782ed
Marie <mariemforte@gmail.com>
2016-10-29 10:24:02 +0000

    Ajout version image sur joystick.py

--- joystick.py
+++ joystick.py
@@ -1,4 +1,4 @@
-
+import math
 
 from sense_hat import SenseHat
 sense=SenseHat()
@@ -21,16 +21,39 @@ while(True):
             pressure=str(round (sense.get_pressure(),0))
             humidity=str(round (sense.get_humidity(),2))
             
-
-
-
-            
             sense.show_message("temperature: ",0.15)
             sense.show_message(temp+"C",0.15,[0,0,255])
             sense.show_message("humidite:",0.15)
             sense.show_message(humidity+"%",0.15,[0,0,255])
             sense.show_message("pression:",0.15)
             sense.show_message(pressure+"hPa",0.15,[0,0,255])
+        elif affichage=='image':
+            if sense.pressure >= 1023: 
+                sense.load_image("soleil.png")
+            elif sense.pressure >= 1013:
+                sense.load_image("variable.png")
+            elif sense.pressure >= 1000:
+                sense.load_image("nuage.png")
+            else:
+                sense.load_image ("pluie.png")
+                
+
+            sense.set_pixel(0,7,0,255,0)
+                
+                
+            sense.set_pixel(0,7,0,255,00);
+            nbpix=sense.humidity/100*6+1;
+            print (nbpix)
+            nbpixint=math.floor(nbpix)
+            print(nbpixint)
+            sense.set_pixel(nbpixint,7,0,255,0);
+
+            counter = 0;
+            while (counter <= nbpixint):
+                print (counter)
+                sense.set_pixel(counter,7,0,255,0);
+                counter = counter +1;
+
 
         print("bouton relaché")
         
