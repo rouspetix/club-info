e43928f13f4d5cdeb05b1a691f1acd65fab2df93
mister1610 <mmister413@gmail.com>
2017-03-21 20:33:05 +0000

    Ajout Joystick, WIP

--- joystick.py
+++ joystick.py
@@ -4,6 +4,35 @@ from sense_hat import SenseHat
 sense=SenseHat()
 
 
+affichage='image'
 
-event = sense.stick.wait_for_event()
-print("The joystick was {} {}".format(event.action, event.direction))
+while(True):
+
+    
+    event = sense.stick.wait_for_event()
+    
+    if event.action=='released': #condition relachement
+        if affichage=='texte':
+            print (sense.pressure)
+            print(sense.temperature)
+            print(sense.humidity)
+
+        print("bouton relaché")
+        
+        if affichage=='image': #condition de l'affichage
+            affichage='texte'
+        elif affichage=='texte': #condition du texte
+            affichage='image'
+
+            print(affichage)
+
+    
+
+
+
+
+
+
+#(==) comparaison
+#(=) affectation se qui est a droite, on le met a gauche
+#print("The joystick was {} {}".format(event.action, event.direction))
