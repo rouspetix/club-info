593ac5453cd2f843c36363847aa9718439cd5442
mister1610 <mmister413@gmail.com>
2017-07-08 10:35:36 +0000

    Finition de la boussole

--- boussole.py
+++ boussole.py
@@ -1,18 +1,10 @@
 from sense_hat import SenseHat
 from math import *
+from time import sleep
 
 sense = SenseHat ()
 
-sense.set_imu_config(False, False, False)  # compass only
-
-north = sense.get_compass()
-print("North: %s" % north)
-
-
-raw = sense.get_compass_raw()
-print("x: {x}, y: {y}, z: {z}".format(**raw))
-
-# Reinitialisation de la matrice
+sense.set_imu_config(True, True, True)  # compass only
 
 x = 0
 while (x <= 7):
@@ -20,36 +12,33 @@ while (x <= 7):
     while (y <= 7):
         sense.set_pixel(x, y, 0,0,0)
         y = y + 1
-    x = x + 1
-
-# initalisation axes
-counter = 0
-while (counter <= 6):
-    sense.set_pixel(counter, 6, 48, 48, 48)
-    sense.set_pixel(counter, 0, 48, 48, 48)
-    sense.set_pixel(0, counter, 48, 48, 48)
-    sense.set_pixel(6, counter, 48, 48, 48)
-
-    counter = counter+1
-
-sense.set_pixel(3, 0, 255, 48, 48)
-
-a=north
-x=cos(a/180*pi+pi/2)*3+3
-y=sin(a/180*pi+pi/2)*3+3
-
-#x=cos(a/360*pi)
-#y=sin(a/360)
-
-print("x:",x)
-print("y:",y)
-sense.set_pixel(x,y, 15,15,255)
-
-
-#print("Pi : ", pi);
-#print("cos pi : ", cos(pi));
+        x = x + 1
 
 
 
+x = 0
+y = 0
+# Reinitialisation de la matrice
+while True:
 
-#print "test cos(180) : ",  cos(180/360)
+    sense.set_pixel(x,y, 0,0,0)
+
+    # initalisation bordures
+    counter = 0
+    while (counter <= 6):
+        sense.set_pixel(counter, 6, 48, 48, 48)
+        sense.set_pixel(counter, 0, 48, 48, 48)
+        sense.set_pixel(0, counter, 48, 48, 48)
+        sense.set_pixel(6, counter, 48, 48, 48)
+
+        counter = counter+1
+
+
+    north = sense.get_compass()
+
+    x=cos(north/180*pi+pi/2)*3+3
+    y=sin(north/180*pi+pi/2)*3+3
+    sense.set_pixel(x,y, 255,48, 48)
+
+    print("north:",north)
+    #sleep(0.5)
